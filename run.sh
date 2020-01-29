533b088d38adcac3a6eb75ba2c7dd9c73e9efacc
Cliff Ingham <inghamn@bloomington.in.gov>
2020-01-29 09:38:27 -0500

    Fixed startup script

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 source /etc/default/grafana-server
 
-exec grafana-server \
-    --homepath=$GRAFANA_HOME \
-    --config=$CONF_FILE \
-    --packaging=docker
+exec grafana-server                                        \
+    --homepath=$GRAFANA_HOME                               \
+    --config=$CONF_FILE                                    \
+    --packaging=docker                                     \
+    "$@"                                                   \
+    cfg:default.log.mode="console"                         \
+    cfg:default.paths.data="$DATA_DIR"                     \
+    cfg:default.paths.logs="$LOG_DIR"                      \
+    cfg:default.paths.plugins="$PLUGINS_DIR"               \
+    cfg:default.paths.provisioning="$PROVISIONING_CFG_DIR"
